717b71a5bebc4ac48a0c0b2324991f0dd679ce9c
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2025-01-28 09:23:21 +0100

    Adds new logic to the number display and refactored html file

--- numberToLcd/main.js
+++ numberToLcd/main.js
@@ -10,8 +10,28 @@ inputElement.addEventListener("input", () => {
   document.getElementById("lcd-line-two").innerHTML = lcdParser.getSecondLine();
   document.getElementById("lcd-line-three").innerHTML =
     lcdParser.getThirdLine();
+  groupCharacters(lcdParser.firstLine,"grouped-text-one");
+  groupCharacters(lcdParser.secondLine,"grouped-text-two");
+  groupCharacters(lcdParser.thirdLine,"grouped-text-three");
 });
 
-window.validateNumberLength = function(input) {
+window.validateNumberLength = function (input) {
   input.value = input.value.replace(/[^0-9]/g, "");
+};
+
+function groupCharacters(inputString, elementId) {
+  const container = document.getElementById(elementId);
+  let groupedString = "";
+
+  // Split the string into groups of three characters
+  for (let i = 0; i < inputString.length; i += 3) {
+    groupedString += `<span class="group">${inputString.slice(
+      i,
+      i + 3
+    )}</span>`;
+  }
+
+  container.innerHTML = groupedString;
 }
+
+// Example usage
